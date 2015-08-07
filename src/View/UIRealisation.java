814fe8e6fa28659d21ab465f3c23db225f489597
Igor <igaleksus@yandex.ru>
2015-08-08 01:56:08 +0300

    Я сделал что-то

--- src/View/UIRealisation.java
+++ src/View/UIRealisation.java
@@ -2,6 +2,7 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.peer.LabelPeer;
 
 /**
  * Created by mac on 08.06.15.
@@ -12,6 +13,8 @@ public class UIRealisation implements UI {
     private JPanel mainPanel;
     private JPanel[] lines;
     private JPanel[] cells;
+    private JPanel[] downPanels;
+    private JPanel[] centerPanels;
 
     @Override
     public void init() {
@@ -20,7 +23,7 @@ public class UIRealisation implements UI {
         lines = new JPanel[4];
         cells = new JPanel[16];
         mainPanel = new JPanel();
-
+        downPanels = new JPanel[16];
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
         for (int i = 0; i < lines.length; i++){
@@ -31,7 +34,7 @@ public class UIRealisation implements UI {
 
         for (int i = 0; i < cells.length; i++){
             cells[i] = new JPanel();
-            cells[i].setLayout(new GridBagLayout());
+            cells[i].setLayout(new BorderLayout());
             if (i < 4 && i >= 0){
                 lines[0].add(cells[i]);
             } else if (i < 8 && i >= 4){
@@ -43,7 +46,11 @@ public class UIRealisation implements UI {
             }
         }
 
-
+        for (int i = 0; i < downPanels.length; i++){
+            downPanels[i] = new JPanel();
+            downPanels[i].setLayout(new BoxLayout(downPanels[i], BoxLayout.X_AXIS));
+//            downPanels[i].add(new JPanel());
+        }
 
         for (int i = 0; i < 16; i++){
             buttons[i] = new JButton[7];
@@ -51,30 +58,47 @@ public class UIRealisation implements UI {
                 buttons[i][b] = new JButton();
                 switch (b){
                     case 0:
-                        cells[i].add(buttons[i][b], new GridBagConstraints(2, 3, 1, 4, 3, 3, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+                        JPanel lPanel = new JPanel();
+                        lPanel.setLayout(new GridBagLayout());
+                        lPanel.add(new JPanel(), new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        lPanel.add(buttons[i][b], new GridBagConstraints(0, 1, 1, 1, 1, 2, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+                        lPanel.add(new JPanel(), new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        cells[i].add(lPanel, BorderLayout.CENTER);
                         break;
                     case 1:
-                        cells[i].add(new JPanel(), new GridBagConstraints(1, 2, 1, 1, 0, 2, GridBagConstraints.ABOVE_BASELINE, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
-                        cells[i].add(new JPanel(), new GridBagConstraints(0, 2, 1, 0, 0, 0, GridBagConstraints.ABOVE_BASELINE, GridBagConstraints.NONE, new Insets(0, 1, 0, 0), 0, 0));
-                        cells[i].add(buttons[i][b], new GridBagConstraints(1, 3, 1, 1, 0, 3, GridBagConstraints.ABOVE_BASELINE, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
+                        JPanel sPanel = new JPanel();
+                        sPanel.setLayout(new BoxLayout(sPanel, BoxLayout.Y_AXIS));
+                        sPanel.add(new JPanel());
+                        sPanel.add(buttons[i][b]);
+                        sPanel.add(new JPanel());
+                        cells[i].add(sPanel, BorderLayout.WEST);
                         break;
                     case 2:
-//                        cells[i].add(new JPanel(), new GridBagConstraints(1, 0, 2, 1, 2, 1, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0, 4, 0, 0), 0, 0));
-                        cells[i].add(buttons[i][b], new GridBagConstraints(2, 0, 1, 3, 3, 3, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 50, 0, 0), 0, 0));
+                        JPanel rPanel = new JPanel();
+                        rPanel.setLayout(new BoxLayout(rPanel, BoxLayout.X_AXIS));
+                        rPanel.add(new JPanel());
+                        rPanel.add(buttons[i][b]);
+                        rPanel.add(new JPanel());
+                        cells[i].add(rPanel, BorderLayout.NORTH);
                         break;
                     case 3:
-                        cells[i].add(buttons[i][b], new GridBagConstraints(4, 2, 1, 2, 1, 1, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        JPanel yPanel = new JPanel();
+                        yPanel.setLayout(new BoxLayout(yPanel, BoxLayout.Y_AXIS));
+                        yPanel.add(new JPanel());
+                        yPanel.add(buttons[i][b]);
+                        yPanel.add(new JPanel());
+                        cells[i].add(yPanel, BorderLayout.EAST);
                         break;
                     case 4:
-                        cells[i].add(buttons[i][b], new GridBagConstraints(4, 4, 1, 2, 1, 1, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        downPanels[i].add(buttons[i][b]);
+                        cells[i].add(downPanels[i], BorderLayout.SOUTH);
                         break;
                     case 5:
-                        cells[i].add(buttons[i][b], new GridBagConstraints(3, 4, 1, 2, 1, 1, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        downPanels[i].add(buttons[i][b]);
                         break;
                     case 6:
-                        cells[i].add(buttons[i][b], new GridBagConstraints(2, 4, 1, 2, 1, 1, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+                        downPanels[i].add(buttons[i][b]);
                         break;
-
                 }
             }
         }
